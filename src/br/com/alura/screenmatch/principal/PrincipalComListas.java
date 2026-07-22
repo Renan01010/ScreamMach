e8344db55fc508309388c5e7a3be7a12d52b917f
Renan Teles <renantelesdeoliveira@gmail.com>
2026-07-22 20:08:55 -0300

    Definindo outros critérios de ordenação

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -45,6 +46,10 @@ public class PrincipalComListas {
 
         Collections.sort(lista);
         System.out.println("Lista de titulos ordenadas: ");
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
         System.out.println(lista);
 
     }
